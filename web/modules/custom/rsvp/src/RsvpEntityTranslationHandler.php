1ca2735405195bcdb3b4d08a2fddfaa5686398f7
Igor Barato <igor.barato@gmail.com>
2020-01-13 09:36:34 -0300

    Basic files and custom modules and themes

---
+++ web/modules/custom/rsvp/src/RsvpEntityTranslationHandler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\rsvp;
+
+use Drupal\content_translation\ContentTranslationHandler;
+
+/**
+ * Defines the translation handler for rsvp_entity.
+ */
+class RsvpEntityTranslationHandler extends ContentTranslationHandler {
+
+  // Override here the needed methods from ContentTranslationHandler.
+}
